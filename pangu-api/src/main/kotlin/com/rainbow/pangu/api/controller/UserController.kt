cc65bee19b2caabcd6f0a1db128eef6db3f9e716
chenyingzhou <echocyz@gmail.com>
2022-10-16 16:52:09 +0800

    增加文档描述

--- pangu-api/src/main/kotlin/com/rainbow/pangu/api/controller/UserController.kt
+++ pangu-api/src/main/kotlin/com/rainbow/pangu/api/controller/UserController.kt
@@ -25,14 +25,19 @@ class UserController {
     lateinit var userService: UserService
 
     @PostMapping("/sms/code")
-    @Operation(summary = "发送验证码(返回脱敏手机号)")
+    @Operation(summary = "发送验证码", description = "用于注册、重置密码。在登录状态下，手机号不必传")
     fun sendCode(@RequestBody sendSmsParam: SendSmsParam): ResultBody<String> {
         val userId = ClientInfoHolder.userId
         return ResultBody.ok(userService.sendCode(userId, sendSmsParam.phoneNo))
     }
 
     @PostMapping("/user/login")
-    @Operation(summary = "登录(密码和验证码二选一)")
+    @Operation(
+        summary = "登录",
+        description = "未注册的，使用<b>手机号+验证码</b>注册并登录；<br>" +
+                "已注册的，使用<b>手机号+密码</b>登录；<br>" +
+                "已注册但忘记密码的，使用<b>手机号+验证码+重置标识</b>登录，服务端会清除密码"
+    )
     fun login(@RequestBody loginParam: LoginParam): ResultBody<LoginVO> {
         val vo = userService.login(loginParam.phoneNo, loginParam.password, loginParam.code, loginParam.resetPassword)
         return ResultBody.ok(vo)
@@ -61,7 +66,7 @@ class UserController {
     }
 
     @PostMapping("/user/password")
-    @Operation(summary = "修改密码(原密码和验证码二选一)")
+    @Operation(summary = "修改密码", description = "原密码和验证码二选一")
     @LoginCheck(lock = true)
     fun changePassword(@RequestBody changePasswordParam: ChangePasswordParam): ResultBody<Boolean> {
         changePasswordParam.userId = ClientInfoHolder.userId
