db8c4cb64439169c639ffa56bc2787b1b8268b72
chenyingzhou <echocyz@gmail.com>
2022-10-26 17:56:02 +0800

    一级市场开始前将资产加入可售池

---
+++ pangu-api/src/main/kotlin/com/rainbow/pangu/api/schedule/UnsoldSchedule.kt
@@ -0,0 +1,36 @@
+package com.rainbow.pangu.api.schedule
+
+import com.rainbow.pangu.annotation.ScheduleLocker
+import com.rainbow.pangu.api.service.UnsoldService
+import com.rainbow.pangu.repository.GoodsItemRepo
+import com.rainbow.pangu.repository.GoodsRepo
+import org.springframework.data.domain.Pageable
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import javax.annotation.Resource
+
+@Component
+class UnsoldSchedule {
+    @Resource
+    lateinit var goodsRepo: GoodsRepo
+
+    @Resource
+    lateinit var goodsItemRepo: GoodsItemRepo
+
+    @Resource
+    lateinit var unsoldService: UnsoldService
+
+    @Scheduled(cron = "0 5 * * * ?")
+//    @Scheduled(fixedDelay = 1L)
+    @ScheduleLocker
+    fun add() {
+        val now = LocalDateTime.now()
+        val goodsList = goodsRepo.findAll().filter { now < it.primaryTime } //列出一级市场尚未开启的商品
+        for (goods in goodsList) {
+            val goodsItems = goodsItemRepo.findAllByGoodsId(goods.id, Pageable.unpaged())
+            val goodsItemIds = goodsItems.map { it.id }
+            unsoldService.add(goods.id, goodsItemIds)
+        }
+    }
+}
